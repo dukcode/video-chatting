8f3d6fe34ca609d593c0d7a138e58e518dc29873
김덕윤 <duk9741@gmail.com>
2024-10-24 17:10:49 +0900

    init project

---
+++ src/main/java/com/example/video_chatting/VideoChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.video_chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoChattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/video_chatting/VideoChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
